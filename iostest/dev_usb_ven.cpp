2496a4bbb3dc02d59e9f65125086e4db954891bc
Léo Lam <leo@innovatetechnologi.es>
2016-11-15 00:13:36 +0100

    Add more tests for VEN (device change)

--- iostest/dev_usb_ven.cpp
+++ iostest/dev_usb_ven.cpp
@@ -30,7 +30,9 @@ void TestGETDEVPARAMS(const s32 fd, const s32 device_id)
   memset(buffer, 0, 0xC0);
   memset(in_buffer, 0, 0x20);
   in_buffer[0] = device_id;
-  in_buffer[1] = 1;
+  // in_buffer[2] = 0x02000001;
+
+  network_printf("in buffer:\n%s", ArrayToString(reinterpret_cast<u8*>(in_buffer), 0x20, 4).c_str());
 
   const s32 ret = IOS_Ioctl(fd, 3, in_buffer, 0x20, buffer, 0xC0);
   if (ret < 0)
@@ -42,6 +44,34 @@ void TestGETDEVPARAMS(const s32 fd, const s32 device_id)
   network_printf("buffer:\n%s", ArrayToString(reinterpret_cast<u8*>(buffer), 0xc0, 4).c_str());
 }
 
+void TestDEVICECHANGE(const s32 fd)
+{
+  network_printf("ioctl USBV5_IOCTL_GETDEVICECHANGE\n");
+  network_printf("note: this will block until a device is plugged in or removed,\n");
+  network_printf("unless this is the first GETDEVICECHANGE ioctl\n");
+
+  u8* buffer = (u8*)iosAlloc(hId, 0x180);
+  if (!buffer)
+  {
+    network_printf("failed to alloc buffer\n");
+    return;
+  }
+  memset(buffer, 0, 0x180);
+
+  const s32 ret = IOS_Ioctl(fd, USBV5_IOCTL_GETDEVICECHANGE, nullptr, 0, buffer, 0x180);
+  network_printf("ioctl(fd, USBV5_IOCTL_GETDEVICECHANGE) ret = %d\n", ret);
+  if (ret < 0)
+  {
+    return;
+  }
+
+  network_printf("devices:\n%s", ArrayToString(reinterpret_cast<u8*>(buffer), 0xc0, sizeof(usb_device_entry)).c_str());
+  iosFree(hId, buffer);
+
+  network_printf("ioctl USBV5_IOCTL_ATTACHFINISH\n");
+  const s32 ret2 = IOS_Ioctl(fd, USBV5_IOCTL_ATTACHFINISH, nullptr, 0, nullptr, 0);
+  network_printf("ioctl(fd, USBV5_IOCTL_ATTACHFINISH) ret = %d\n", ret2);
+}
 
 int main()
 {
@@ -61,23 +91,23 @@ int main()
     return IPC_ENOMEM;
   }
 
-  auto* descr_buffer = static_cast<usb_device_entry*>(iosAlloc(hId, 8 * sizeof(usb_device_entry)));
-  if (!descr_buffer) {
-    network_printf("Failed to allocate buffer");
-    return 1;
-  }
+  // auto* descr_buffer = static_cast<usb_device_entry*>(iosAlloc(hId, 8 * sizeof(usb_device_entry)));
+  // if (!descr_buffer) {
+  //   network_printf("Failed to allocate buffer");
+  //   return 1;
+  // }
 
-  memset(descr_buffer, 0, 8 * sizeof(usb_device_entry));
+  // memset(descr_buffer, 0, 8 * sizeof(usb_device_entry));
 
-  u8 cnt_descr;
-  USB_Initialize();
-  const s32 ret = USB_GetDeviceList(descr_buffer, 8, 8, &cnt_descr);
-  if (ret < 0)
-  {
-    network_printf("failed to get list: %d\n", ret);
-    return 1;
-  }
-  USB_Deinitialize();
+  // u8 cnt_descr;
+  // USB_Initialize();
+  // const s32 ret = USB_GetDeviceList(descr_buffer, 8, 8, &cnt_descr);
+  // if (ret < 0)
+  // {
+  //   network_printf("failed to get list: %d\n", ret);
+  //   return 1;
+  // }
+  // USB_Deinitialize();
 
   const int fd = IOS_Open("/dev/usb/ven", IPC_OPEN_NONE);
   if (fd < 0)
@@ -99,26 +129,32 @@ int main()
   }
   iosFree(hId, ven_ver);
 
-  for (int i = 0; i < cnt_descr; ++i)
+  // for (int i = 0; i < cnt_descr; ++i)
+  // {
+  //   const auto& entry = descr_buffer[i];
+  //   network_printf("entry: device_id %x vid %x pid %x token %x\n", entry.device_id, entry.vid, entry.pid, entry.token);
+
+  //   // Resume device
+  //   s32* buf = (s32*)iosAlloc(hId, 32);
+  //   if (!buf)
+  //   {
+  //     network_printf("failed to alloc buf to resume device\n");
+  //     return 1;
+  //   }
+  //   buf[0] = entry.device_id;
+  //   buf[2] = 1;
+  //   const s32 resume_ret = IOS_Ioctl(fd, USBV5_IOCTL_SUSPEND_RESUME, buf, 32, nullptr, 0);
+  //   network_printf("resume_ret = %d\n", resume_ret);
+  //   iosFree(hId, buf);
+
+  //   TestGETDEVPARAMS(fd, entry.device_id);
+  //   network_printf("\n");
+  // }
+
+  for (int i = 0; i < 5; ++i)
   {
-    const auto& entry = descr_buffer[i];
-    network_printf("entry: device_id %x vid %x pid %x token %x\n", entry.device_id, entry.vid, entry.pid, entry.token);
-
-    // Resume device
-    s32* buf = (s32*)iosAlloc(hId, 32);
-    if (!buf)
-    {
-      network_printf("failed to alloc buf to resume device\n");
-      return 1;
-    }
-    buf[0] = entry.device_id;
-    buf[2] = 1;
-    const s32 resume_ret = IOS_Ioctl(fd, USBV5_IOCTL_SUSPEND_RESUME, buf, 32, nullptr, 0);
-    network_printf("resume_ret = %d\n", resume_ret);
-    iosFree(hId, buf);
-
-    TestGETDEVPARAMS(fd, entry.device_id);
-    network_printf("\n");
+    network_printf("TestDEVICECHANGE #%d\n", i);
+    TestDEVICECHANGE(fd);
   }
 
   IOS_Close(fd);
